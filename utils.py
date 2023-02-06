c43ed1584bd704e335d11437c573798f01b0edb5
Farid Davletshin <118741179+davdevv@users.noreply.github.com>
2023-02-07 01:40:42 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,22 +4,6 @@ import matplotlib.pyplot as plt
 import os
 
 
-def disable_running_stats(model):
-    def _disable(module):
-        if isinstance(module, _BatchNorm):
-            module.backup_momentum = module.momentum
-            module.momentum = 0
-
-    model.apply(_disable)
-
-
-def enable_running_stats(model):
-    def _enable(module):
-        if isinstance(module, _BatchNorm) and hasattr(module, "backup_momentum"):
-            module.momentum = module.backup_momentum
-
-    model.apply(_enable)
-
 
 def print_metrics(loss, acc):
     epoch = len(loss['train'])
